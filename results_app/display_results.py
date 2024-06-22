8c3403f9a0fb699038557f58b445de30d61b9ce2
jclachance <jelachance@eng.ucsd.edu>
2024-06-22 12:59:50 -0400

    Functional app

--- results_app/display_results.py
+++ results_app/display_results.py
@@ -7,16 +7,50 @@ st.title('Display metabolic modeling results')
 
 st.markdown("To start using this app, select a model from the sidebar. The scrolldown will show you the models stored under results_app/models, make sure it is not empty!")
 
-st.sidebar.markdown('## Select your modeling options here')
-
 MODEL_PATH = './models'
 
-model_file = st.sidebar.selectbox(
-    'Select a model',
-    os.listdir(MODEL_PATH)
-)
+with st.sidebar.form(key='sidebar_selection_form'):
+    st.markdown('## Select your modeling options here')
+    model_file = st.selectbox(
+        'Select a model',
+        os.listdir(MODEL_PATH)
+        )
+    st.markdown('## Select your target metabolite here')
+    target_metabolite = st.selectbox(
+        'Select a target metabolite',
+        ('S_Cellulose_c', 'S_Pectin_c')
+    )
+    st.markdown('## Select the number of samples here')
+    number_of_samples = st.number_input(
+        'Number of samples',
+        min_value = 50,
+        max_value = 10000
+    )
+    modeling_options_submitted = st.form_submit_button(label = 'Submit modeling options')
 
-model_load_state = st.text('Loading metabolic model...')
-model = get_model(os.path.join(MODEL_PATH, model_file))
-model_load_state.text('Loading metabolic model... Done!')
-
+if modeling_options_submitted:
+    # 1. Load model
+    model_load_state = st.text('Loading metabolic model...')
+    model = get_model(os.path.join(MODEL_PATH, model_file))
+    model_load_state.text('Loading metabolic model... Done!')    
+    # 2. Perform flux sampling
+    flux_sampling_state = st.text('Performing flux sampling...')
+    old_objective = 'Ex16'
+    new_objective = 'BIO_L'
+    model = change_model_objective(old_objective, new_objective, model)
+    # 2.1 Generate biomass flux samples
+    biomass_samples = run_flux_sampling(model, n_samples=number_of_samples)
+    flux_sampling_state.text('Performing flux sampling... Biomass flux sampling is done!')
+    # 2.2 Generate target flux samples
+    model, new_objective = create_exchange_reaction(model, target_metabolite)
+    model = change_model_objective(old_objective, new_objective, model)
+    target_samples = run_flux_sampling(model, n_samples=50)
+    flux_sampling_state.text('Performing flux sampling... Target flux sampling is done!')
+    # 3. Perform Kolmogorov-Smirnov test to get distribution of flux samples
+    ks_test = perform_ks_test(model, biomass_samples, target_samples)
+    flux_sampling_state.text('Performing flux sampling... Kolmogorov-Smirnov test is done!')
+    # 4. Display results
+    top_hits = get_top_hits(ks_test, n_hits=9)
+    st.dataframe(top_hits)
+    # 5. Plot distribution
+    plot_distribution(top_hits['Reaction'].to_list(), biomass_samples, target_samples)
